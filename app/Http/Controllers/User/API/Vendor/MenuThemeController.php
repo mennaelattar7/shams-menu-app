22f014438efab25bb4b70f54e154e9b06ec4483e
mennaelattar <mennaelattar3@gmail.com>
2026-02-22 11:13:42 +0200

    .

--- app/Http/Controllers/User/API/Vendor/MenuThemeController.php
+++ app/Http/Controllers/User/API/Vendor/MenuThemeController.php
@@ -7,9 +7,21 @@ use Illuminate\Http\Request;
 
 class MenuThemeController extends BaseController
 {
-    public function update()
+    public function update(Request $request)
     {
         $vendor = $this->vendor;
-        dd($vendor->menu_theme);
+        $menu_theme = $vendor->menu_theme;
+        $menu_theme_details = $menu_theme->menu_theme_details;
+        dd($menu_theme_details);
+        $menu_theme->updated_by_id = $this->user->id;
+        $menu_theme->main_category_layout = $request->main_category_layout;
+        $menu_theme->products_layout = $request->products_layout;
+        $menu_theme->theme_name = $request->theme_name;
+        $menu_theme->save();
+        if($request->theme_name == "light")
+        {
+
+        }
+
     }
 }
--- app/Models/Vendor__MenuTheme.php
+++ app/Models/Vendor__MenuTheme.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Vendor__MenuTheme extends Model
 {
     protected $table = "vendor___menu_themes";
+
+    public function menu_theme_details()
+    {
+        return $this->hasOne(Vendor__MenuThemeDetail::class,'menu_theme_id','id');
+    }
 }
