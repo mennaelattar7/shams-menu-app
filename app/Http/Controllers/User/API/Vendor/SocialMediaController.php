108064feeaeb5f1c1040995370bf561493fe17be
mennaelattar <mennaelattar3@gmail.com>
2026-03-11 11:03:21 +0200

    .

--- app/Http/Controllers/User/API/Vendor/SocialMediaController.php
+++ app/Http/Controllers/User/API/Vendor/SocialMediaController.php
@@ -11,6 +11,14 @@ class SocialMediaController extends BaseController
     public function index()
     {
         $social_media = $this->vendor->social_media;
+        if(!$social_media)
+        {
+            return response()->json([
+                'success' =>true,
+                'message' => 'There Are no Social media',
+                'data' => []
+            ],200);
+        }
         return response()->json([
             'success' =>true,
             'message' => 'get Social Media successfully',
