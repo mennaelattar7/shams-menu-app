69d425d585e18409218fc3b60f2f672611c6fecf
mennaelattar <mennaelattar3@gmail.com>
2026-03-16 12:05:48 +0200

    .

--- app/Http/Controllers/User/API/Vendor/Reports/ProductViews/ProductTrackingController.php
+++ app/Http/Controllers/User/API/Vendor/Reports/ProductViews/ProductTrackingController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\User\API\Vendor\Reports\ProductViews;
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\User\API\Vendor\BaseController;
 use App\Models\Product__Tracking;
+use App\Models\Vendor__MenuCategory;
 use App\Models\VendorBranche;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class ProductTrackingController extends BaseController
 {
@@ -51,7 +53,7 @@ class ProductTrackingController extends BaseController
         }
     }
 
-    public function index($locale, $branch_slug)
+    public function index($locale, $branch_slug,Request $request)
     {
         $vendor = $this->vendor;
         $branch = VendorBranche::where('slug',$branch_slug)->first();
@@ -59,9 +61,55 @@ class ProductTrackingController extends BaseController
         {
             $all_products = $branch->products()
                 ->select('products.id','products.name','products.slug')
-                ->selectRaw('(select count(*) from product___trackings where product___trackings.product_id = products.id) as views_count')
-                ->get();
+                ->selectRaw('(select count(*) from product___trackings where product___trackings.product_id = products.id) as views_count');
+            $all_views = Product__Tracking::query();
 
+            if($request->time_period == "this_day" )
+            {
+                $all_views_ids_array = $all_views->whereDate('created_at', Carbon::today())->pluck('product_id')->toArray();
+                $all_products->whereIn('products.id',$all_views_ids_array);
+            }
+            if($request->time_period =="this_week")
+            {
+                $startOfWeek = Carbon::now()->startOfWeek();
+                $endOfWeek = Carbon::now()->endOfWeek();
+                $all_views_ids_array = $all_views->whereBetween('created_at', [$startOfWeek, $endOfWeek])->pluck('product_id')->toArray();
+                $all_products->whereIn('products.id',$all_views_ids_array);
+            }
+            if($request->time_period =="this_month")
+            {
+                $startOfMonth = Carbon::now()->startOfMonth(); // أول يوم في الشهر
+                $endOfMonth   = Carbon::now()->endOfMonth();
+                $all_views_ids_array = $all_views->whereBetween('created_at', [$startOfMonth, $endOfMonth])->pluck('product_id')->toArray();
+                $all_products->whereIn('products.id',$all_views_ids_array);
+            }
+            if($request->time_period =="custom")
+            {
+                $startdate = Carbon::parse($request->start_date)->startOfDay(); // بداية اليوم
+                $enddate   = Carbon::parse($request->end_date)->endOfDay();     // نهاية اليوم 23:59:59
+                $all_views_ids_array = $all_views->whereBetween('created_at', [$startdate, $enddate])->pluck('product_id')->toArray();
+                $all_products->whereIn('products.id',$all_views_ids_array);
+            }
+            if($request->sub_category_slug)
+            {
+                $category = Vendor__MenuCategory::where('slug',$request->sub_category_slug)->first();
+                if($category)
+                {
+                    $all_products = $all_products->where('products.category_id',$category->id);
+                }
+
+            }
+            if ($request->main_category_slug) {
+                $category = Vendor__MenuCategory::where('slug',$request->main_category_slug)->first();
+                if($category)
+                {
+                    $sub_categories_ids = $category->sub_categories()->pluck('id');
+                    $all_products = $all_products->whereIn('products.category_id', $sub_categories_ids);
+                }
+            }
+
+
+            $all_products = $all_products->get();
             if(!$all_products)
             {
                 return response()->json([
